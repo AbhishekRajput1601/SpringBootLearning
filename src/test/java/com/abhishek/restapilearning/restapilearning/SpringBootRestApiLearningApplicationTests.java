f58a7f35cbe94eadf2a946b65adb9fa77d0e3f2a
AbhishekRajput1601 <abhirajputofficial2000@gmail.com>
2026-02-06 11:48:27 +0530

    first commit

---
+++ src/test/java/com/abhishek/restapilearning/restapilearning/SpringBootRestApiLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.restapilearning.restapilearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
